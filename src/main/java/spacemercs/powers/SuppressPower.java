e8fac110783a50bd81429cf5943a8116c6caa496
Jade-Squire <christopherrobbinbare@gmail.com>
2025-11-11 21:16:09 -0700

    Voidwall Grenade complete

---
+++ src/main/java/spacemercs/powers/SuppressPower.java
@@ -0,0 +1,59 @@
+package spacemercs.powers;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+import static spacemercs.SpaceMercsMod.makeID;
+
+public class SuppressPower extends BasePower {
+    public static final String POWER_ID = makeID(SuppressPower.class.getSimpleName());
+    private static final AbstractPower.PowerType TYPE = PowerType.BUFF;
+    private static final boolean TURN_BASED = false;
+
+    public SuppressPower(AbstractCreature owner, int amount) {
+        super(POWER_ID, TYPE, TURN_BASED, owner, amount);
+    }
+
+    public int onAttacked(DamageInfo info, int damageAmount) {
+        int weakStacks = 0;
+        int vulnStacks = 0;
+        if(info.output > 0) {
+            if(owner.hasPower(WeakPower.POWER_ID)) {
+                weakStacks = owner.getPower(WeakPower.POWER_ID).amount;
+            }
+            if(owner.hasPower(VulnerablePower.POWER_ID)) {
+                vulnStacks = owner.getPower(VulnerablePower.POWER_ID).amount;
+            }
+            if(weakStacks == vulnStacks) {
+                if(Math.random() >= 0.5) {
+                    addToTop(new ApplyPowerAction(owner, AbstractDungeon.player, new VulnerablePower(owner, 1, false)));
+                } else {
+                    addToTop(new ApplyPowerAction(owner, AbstractDungeon.player, new WeakPower(owner, 1, false)));
+                }
+            } else if (weakStacks < vulnStacks) {
+                addToTop(new ApplyPowerAction(owner, AbstractDungeon.player, new WeakPower(owner, 1, false)));
+            } else {
+                addToTop(new ApplyPowerAction(owner, AbstractDungeon.player, new VulnerablePower(owner, 1, false)));
+            }
+        }
+        if(owner.hasPower(POWER_ID)) {
+            if(owner.getPower(POWER_ID).amount > 1) {
+                addToBot(new ReducePowerAction(owner, owner, this, 1));
+            } else {
+                addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+            }
+        }
+        return damageAmount;
+    }
+
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0];
+    }
+}
--- src/main/java/spacemercs/powers/VoidwallPower.java
+++ src/main/java/spacemercs/powers/VoidwallPower.java
@@ -5,7 +5,6 @@ import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import static spacemercs.SpaceMercsMod.makeID;
 
@@ -23,13 +22,13 @@ public class VoidwallPower extends BasePower implements OnLoseBlockPower {
     }
 
     public void updateDescription() {
-        this.description = DESCRIPTIONS[0];
+        this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
     }
 
     @Override
     public int onLoseBlock(DamageInfo damageInfo, int i) {
         if(damageInfo.owner != this.owner) {
-            addToBot(new ApplyPowerAction(damageInfo.owner, owner,new VulnerablePower(damageInfo.owner, 1, false)));
+            addToBot(new ApplyPowerAction(damageInfo.owner, owner,new SuppressPower(damageInfo.owner, amount)));
         }
         return i;
     }
